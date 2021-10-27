59830d6b5e48611f77db0fdca1530f143ebe71c1
Gr33nLight <bdepaula520@gmail.com>
2021-10-27 21:30:37 +0200

    chore: add Mocks for local ganache-cli

--- scripts/deplou.py
+++ scripts/deplou.py
@@ -1,19 +1,23 @@
-from scripts.utils import get_account
-from brownie import network, config, FundMe
+from scripts.utils import get_account, deploy_mocks
+from brownie import network, config, FundMe, MockV3Aggregator
 
 
 def deploy_fund_me():
     account = get_account()
+    is_dev = network.show_active() == "development"
 
-    if network.show_active() != "development":
+    if not is_dev:
         price_feed_addr = config["networks"][network.show_active()]["price_feed_addr"]
     else:
-        pass
+        print("Deploying Mocks...")
+        deploy_mocks()
+        price_feed_addr = MockV3Aggregator[-1].address
+        print("Mocks deployed")
 
     fund_me = FundMe.deploy(
         price_feed_addr,
         {"from": account},
-        publish_source=True,
+        publish_source=(config["networks"][network.show_active()].get("verify")),
     )
     print(f"Contract deployed to {fund_me.address}")
 
